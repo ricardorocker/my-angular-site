0faad812aebbb8f9b166db31415a96948bb75c32
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-04 20:56:35 -0300

    Update cardComponent and reactiveFormComponent

--- src/app/views/projects/projects.component.ts
+++ src/app/views/projects/projects.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
-  templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.css']
+  templateUrl: './projects.component.html'
 })
 export class ProjectsComponent {
 
