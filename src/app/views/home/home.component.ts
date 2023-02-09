27c82a7e2a02b2781b1e99b5987fb4b9f0640399
Ricardo Rocker <76121782+ricardorocker@users.noreply.github.com>
2023-02-09 00:51:44 -0300

    Update background home, navbar style, buttons to bootstrap

--- src/app/views/home/home.component.ts
+++ src/app/views/home/home.component.ts
@@ -11,7 +11,7 @@ export class HomeComponent {
   constructor(private router: Router) {}
 
   goToStudies(): void {
-    this.router.navigate(['/projects']);
+    this.router.navigate(['/experience']);
   }
 
 }
